d142e7e53b3a27d3e92db97c7b95da4607ed0a4d
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-12-31 13:20:43 -0600

    Added ActiveModelSerializers gem tu comply with json:api specification

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -11,8 +11,18 @@ describe ArticlesController do
       create_list(:article, 2)
       get :index
       json = JSON.parse(response.body)
-      #json_data = json[:data]
-      #expect(json_data.length).to eq(2)
+      json_data = json['data']
+      expect(json_data.length).to eq(2)
+      expect(json_data[0]['attributes']).to eq({
+        "title" => "My article 1",
+        "content" => "This is the content of my article 1",
+        "slug" => "my-article-1"
+      })
+      expect(json_data[1]['attributes']).to eq({
+        "title" => "My article 2",
+        "content" => "This is the content of my article 2",
+        "slug" => "my-article-2"
+      })
     end
   end
 end
