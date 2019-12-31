2f91235b94ae43b68bfd873339f938b2230fd66c
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-12-31 16:28:40 -0600

    Add `recent` scope to articles add small refactorings

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,27 +2,31 @@ require 'rails_helper'
 
 describe ArticlesController do
   describe '#index' do
+    subject { get :index }
+
     it 'should return success response' do
-      get :index
+      subject
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return proper json' do
-      create_list(:article, 2)
-      get :index
-      json = JSON.parse(response.body)
-      json_data = json['data']
-      expect(json_data.length).to eq(2)
-      expect(json_data[0]['attributes']).to eq({
-        "title" => "My article 1",
-        "content" => "This is the content of my article 1",
-        "slug" => "my-article-1"
-      })
-      expect(json_data[1]['attributes']).to eq({
-        "title" => "My article 2",
-        "content" => "This is the content of my article 2",
-        "slug" => "my-article-2"
-      })
+      create_list :article, 2
+      subject
+      Article.recent.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+          "title" => article.title,
+          "content" => article.content,
+          "slug" => article.slug
+        })
+      end
+    end
+
+    it 'should return the articles in the proper order' do
+      old_article = create :article
+      newer_article = create :article
+      subject
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
     end
   end
 end
