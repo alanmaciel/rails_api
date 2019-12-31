198a7c04bdda44d661265dab11c4331c3cdb7268
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-12-31 12:50:09 -0600

    Add article controller and config RSpec to use create_list

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    sequence(:title) { |n| "My article #{n}" }
+    sequence(:content) { |n| "This is the content of my article #{n}" }
+    sequence(:slug) { |n| "my-article-#{n}" }
   end
 end
